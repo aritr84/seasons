88bfbd3f34c2bb56e6c2d0e45a047aa3075d23b2
aritr84 <65041325+aritr84@users.noreply.github.com>
2022-01-08 11:31:51 +0530

    First commit ✅

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import react and reactDOM
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// create a component
+//functional component -
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
+//   return <div></div>;
+// };
+
+// class component
+
+class App extends React.Component {
+  // same as any other constructor - will be called automatically when instance of App is created
+  constructor(props) {
+    super(props); //required
+
+    // initialize the state object
+    // only single time when we do direct assignment
+    this.state = {
+      lat: null,
+      errorMessage: "",
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+
+        // we never call like this -
+        // this.state.lat = position.coords.latitude;
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  componentDidMount() {
+    console.log("My component was rendered to the screen");
+  }
+  componentDidUpdate() {
+    console.log("My component was re rendered to the screen");
+  }
+  // React says we have to define render
+  render() {
+    // return <div> Latitude: {this.state.lat} </div>;
+
+    // conditional rendering -
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage} </div>;
+    }
+
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div> Latitude: {this.state.lat} </div>;
+    }
+    return <div>Loading ...</div>;
+  }
+}
+
+// render using reactDOM
+
+ReactDOM.render(<App />, document.querySelector("#root"));
