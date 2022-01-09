5ca26f6a36d261c2ebb66857d035908494b7a440
aritr84 <65041325+aritr84@users.noreply.github.com>
2022-01-09 09:10:30 +0530

    project 2 completed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 // create a component
 //functional component -
@@ -17,16 +19,22 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component {
   // same as any other constructor - will be called automatically when instance of App is created
-  constructor(props) {
-    super(props); //required
+  // method 1 - state iniltialization inside constructor -
+  // constructor(props) {
+  //   super(props); //required
 
-    // initialize the state object
-    // only single time when we do direct assignment
-    this.state = {
-      lat: null,
-      errorMessage: "",
-    };
+  //   // initialize the state object
+  //   // only single time when we do direct assignment
+  //   this.state = {
+  //     lat: null,
+  //     errorMessage: "",
+  //   };
+  // }
 
+  // method 2 -
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude });
@@ -40,25 +48,23 @@ class App extends React.Component {
     );
   }
 
-  componentDidMount() {
-    console.log("My component was rendered to the screen");
-  }
-  componentDidUpdate() {
-    console.log("My component was re rendered to the screen");
-  }
-  // React says we have to define render
-  render() {
-    // return <div> Latitude: {this.state.lat} </div>;
-
+  // helper function -
+  renderContent() {
     // conditional rendering -
     if (this.state.errorMessage && !this.state.lat) {
       return <div> Error: {this.state.errorMessage} </div>;
     }
 
     if (this.state.lat && !this.state.errorMessage) {
-      return <div> Latitude: {this.state.lat} </div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading ...</div>;
+    return <Spinner message="Please accept location Request" />;
+  }
+
+  // React says we have to define render
+  render() {
+    // return <div> Latitude: {this.state.lat} </div>;
+    return <div className="">{this.renderContent()}</div>;
   }
 }
 
